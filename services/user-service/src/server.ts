b40724f7cdc5ec0bf2e97b5d3bc03373cf98618e
Shreenath Kadam <146519199+Shree7503@users.noreply.github.com>
2025-08-05 23:26:18 +0530

    Initial commit - user-serve and project scaffold

---
+++ services/user-service/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { errorHandler } from './middlewares/errorHandler';
+import config from './config/config';
+
+const app = express();
+
+app.use(express.json());
+
+// Routes
+
+// Global error handler (should be after routes)
+app.use(errorHandler);
+
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
+});

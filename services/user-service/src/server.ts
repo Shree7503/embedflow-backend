08b192527f18eda9d9231be81c7db5489818a88b
My Name <omnikam00007@gmail.com>
2025-08-06 19:15:40 +0530

    mistake fixed

--- services/user-service/src/server.ts
+++ services/user-service/src/server.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(errorHandler);
 
 app.get('/', (req, res) => {
-  res.status(200).send({message: 'fuck you!'});
+  res.status(200).send({message: 'Hello world!'});
 })
 
 app.listen(config.port, () => {

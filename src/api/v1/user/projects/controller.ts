cb8452a44ef19d52792d92eadd2214d6061cbd73
Shreenath Kadam <146519199+Shree7503@users.noreply.github.com>
2025-09-09 22:16:26 +0530

    added model routes

--- src/api/v1/user/projects/controller.ts
+++ src/api/v1/user/projects/controller.ts
@@ -72,7 +72,7 @@ export const updateProject = async (
   res: Response,
   next: NextFunction
 ) => {
-  const id = req.params.id;
+  const id = req.user!.id;
   const updates = req.body;
 
   try {

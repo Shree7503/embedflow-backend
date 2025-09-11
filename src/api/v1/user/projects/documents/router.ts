dc6d727ec0f496107772675f366bc9550a10b725
Shreenath Kadam <146519199+Shree7503@users.noreply.github.com>
2025-09-11 07:31:25 +0530

    indentation

--- src/api/v1/user/projects/documents/router.ts
+++ src/api/v1/user/projects/documents/router.ts
@@ -1,14 +1,20 @@
 import express from "express";
-import {uploadDocument,getDocument,delDocument,changeDocument,getDocumentById,updateDocumentStatus} from "@/api/v1/user/projects/documents/controller";
+import {
+  uploadDocument,
+  getDocument,
+  deleteDocument,
+  changeDocument,
+  getDocumentById,
+  updateDocumentStatus,
+} from "@/api/v1/user/projects/documents/controller";
 
+const router = express.Router({ mergeParams: true });
 
-const router = express.Router({mergeParams:true});
-
-router.post("/",uploadDocument);
-router.get("/",getDocument);
-router.put("/",changeDocument);
-router.delete("/:documentId",delDocument);
-router.get("/:documentId",getDocumentById);
-router.patch("/:documentId",updateDocumentStatus);
+router.post("/", uploadDocument);
+router.get("/", getDocument);
+router.put("/", changeDocument);
+router.delete("/:documentId", deleteDocument);
+router.get("/:documentId", getDocumentById);
+router.patch("/:documentId", updateDocumentStatus);
 
 export default router;
